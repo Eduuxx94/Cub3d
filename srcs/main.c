6db0c8478e6573d2981ac5ed1a4a629d66341a10
zet1r <zet1r@proton.me>
2022-11-30 18:43:15 +0000

    mainf

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jdias-mo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 19:39:46 by ede-alme          #+#    #+#             */
-/*   Updated: 2022/11/30 18:33:26 by jdias-mo         ###   ########.fr       */
+/*   Updated: 2022/11/30 18:42:30 by jdias-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,6 @@ int	main(int argc, char **argv)
 		start_mlx(&game);
 		hook_mlx(&game);
 		mlx_loop(game.mlx.ptr);
-		ft_free_sfile(game.file);
-		return (0);
 	}
 	ft_free_sfile(game.file);
 	return (0);
